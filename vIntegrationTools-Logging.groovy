6bfea300631e0fd33626296b0a17e39a0a331c4e
stefanzimara <129875168+stefanzimara@users.noreply.github.com>
2024-08-07 19:55:44 +0200

    Cleanup of unnecersary Code

--- vIntegrationTools-Logging.groovy
+++ vIntegrationTools-Logging.groovy
@@ -1,7 +1,13 @@
+// -----------------------------------------------------------
 // Datum: 10.03.2022 - Author: Stefan Zimara, Valcoba AG 
 // Version 1.0 
 // Funktion: Log Message with SAP ID 
+// -----------------------------------------------------------
 
+
+
+// -----------------------------------------------------------
+// Libraray Import
 // -----------------------------------------------------------
 import com.sap.gateway.ip.core.customdev.util.*;
 import java.util.HashMap;
@@ -13,7 +19,10 @@ import java.util.Map
 import java.util.Iterator
 import javax.activation.DataHandler
 
+import com.sap.gateway.ip.core.customdev.util.Message;
 
+// -----------------------------------------------------------
+// Call Methods
 // -----------------------------------------------------------
 def Message processData(Message message) {
     log("Default", message, true, true, true, true);
@@ -61,11 +70,82 @@ def Message log_Info(Message message) {
 }
 
 def Message logExceptionMessage(Message message) {
-   log("Exception:", message, true, false, false, false);
+   log("Exception", message, true, false, false, false);
     return message
 }
 
 // -----------------------------------------------------------
+// Main
+// -----------------------------------------------------------
+def Message log(String prefix, Message message,boolean logPayload, boolean logHeaders, boolean logProperties, boolean logSysEnv) {
+
+    def headers = message.getHeaders();
+    def properties = message.getProperties();
+    
+    def enableLoggingValue = properties.get("log.EnableLogging", "TRUE");
+    boolean property_ENABLE_LOGGING = enableLoggingValue?.toString()?.equalsIgnoreCase("true");
+
+    def messageLog = messageLogFactory.getMessageLog(message);
+
+    if (property_ENABLE_LOGGING) {
+        
+        message.setHeader("SAP_IsIgnoreProperties", new Boolean(true));
+        StringBuffer logInfo = new StringBuffer()
+        int logNumber = headers.get("log.Number", "1") as Integer;
+
+        if (prefix == "") {
+            property_prefix = properties.get("log.prefix");
+           
+            if (property_prefix == null || property_prefix.trim() == "") {
+                property_prefix = "Payload"
+            }    
+        } else {
+            property_prefix = prefix
+        }    
+        
+        property_prefix = String.format("%04d", logNumber) + "." + property_prefix
+    
+        // Define Attachment Name
+        def attachmentName = determineAttachmentName(property_prefix, "");
+
+        //Note LogeLevel
+        def logLevel = message.getProperty("SAP_MPL_LogLevel_Overall");
+    
+        //Prepare content, Header Log
+        if(logHeaders) {
+            logInfo.append(logMessageHeaders(message));
+        } 
+
+        //Prepare contentm Property Log
+        if(logProperties) {
+            logInfo.append(logMessageProperties(message));
+        } 
+
+        //Prepare contentm Property Log
+        if(logSysEnv) {
+            logInfo.append(logSystemEnvironment(message));
+        } 
+
+        def body = message.getBody(java.lang.String) as String;
+
+        if(logPayload) {
+            if (logHeaders || logProperties || logSysEnv) {
+                logInfo.append("Payload" + "\n--------------------------\n")
+            }    
+            logInfo.append(body);
+        }
+        
+        logNumber++;
+        message.setHeader("log.Number", logNumber);
+
+        String contentType = determineContentType(logInfo.toString());
+        messageLog.addAttachmentAsString(attachmentName, logInfo.toString(), contentType);
+
+    }
+
+    return message;
+}
+
 
 def Message set_local_time(Message message) {    
     def localTimeZone = TimeZone.getTimeZone('Europe/Berlin');
@@ -108,66 +188,7 @@ def Message fetch_response_error_message_soap(Message message) {
     return message;
 }
 
-/*
-log payload depending on property.ENABLE_LOGGING
-*/
-def Message payload_logger(Message message) {
-    
-    def headers = message.getHeaders();
-    map = message.getProperties();
-    property_ENABLE_LOGGING = map.get("EnableLogging");
-    property_prefix = map.get("log.prefix");
-    int logNumber = headers.get("logNumber", "1") as Integer;
 
-    if (property_prefix == null || property_prefix.trim() == "") {
-        property_prefix = "Payload"
-    }    
-    
-    property_prefix = String.format("%04d", logNumber) + "." + property_prefix
-
-
-    def validValues = ["TRUE", "ALL", "INFO", "DEBUG", "ERROR"]
-
-    if (property_ENABLE_LOGGING != null && !property_ENABLE_LOGGING.trim().isEmpty() && validValues.contains(property_ENABLE_LOGGING.toUpperCase())) {
-        Boolean messageLogged = create_attachment(message, property_prefix)
-    }
-
-    logNumber++;
-    message.setHeader("logNumber", logNumber);
-        
-    return message;
-}
-
-def Message payload_logger_bm(Message message) {
-    
-    map = message.getProperties();
-    property_ENABLE_LOGGING = map.get("EnableLogging");
-    property_prefix = "Before_Mapping";
-
-    def validValues = ["TRUE", "ALL", "INFO", "DEBUG", "ERROR"]
-
-    if (property_ENABLE_LOGGING != null && !property_ENABLE_LOGGING.trim().isEmpty() && validValues.contains(property_ENABLE_LOGGING.toUpperCase())) {
-        Boolean messageLogged = create_attachment(message, property_prefix)
-    }
-
-    return message;
-}
-
-
-def Message payload_logger_am(Message message) {
-    
-    map = message.getProperties();
-    property_ENABLE_LOGGING = map.get("EnableLogging");
-    property_prefix = "After_Mapping";
-
-    def validValues = ["TRUE", "ALL", "INFO", "DEBUG", "ERROR"]
-
-    if (property_ENABLE_LOGGING != null && !property_ENABLE_LOGGING.trim().isEmpty() && validValues.contains(property_ENABLE_LOGGING.toUpperCase())) {
-        Boolean messageLogged = create_attachment(message, property_prefix)
-    }
-
-    return message;
-}
 
 
 def Message logException(Message message) {
@@ -186,165 +207,11 @@ def Message logException(Message message) {
        storeLog(message);
 }
 
-def Message payload_logger_debug(Message message) {
-    
-    map = message.getProperties();
-    property_ENABLE_LOGGING = map.get("EnableLogging");
-    //message.setHeader("SAP_IsIgnoreProperties", new Boolean(true));
 
-    def validValues = ["TRUE", "ALL", "DEBUG"]
-
-    if (validValues.contains(property_ENABLE_LOGGING.toUpperCase())) {
-        Boolean messageLogged = create_attachment(message, "Payload")
-    }
-
-    return message;
-}
-
-def Message payload_logger_error(Message message) {
-    
-    map = message.getProperties();
-    property_ENABLE_LOGGING = map.get("EnableLogging");
-    //message.setHeader("SAP_IsIgnoreProperties", new Boolean(true));
-
-    def validValues = ["TRUE", "ALL", "ERROR", "DEBUG"]
-
-    if (validValues.contains(property_ENABLE_LOGGING.toUpperCase())) {
-        Boolean messageLogged = create_attachment(message, "Payload")
-    }
-
-    return message;
-}
-
-def Message payload_logger_Info(Message message) {
-    
-    map = message.getProperties();
-    property_ENABLE_LOGGING = map.get("EnableLogging");
-    //message.setHeader("SAP_IsIgnoreProperties", new Boolean(true));
-
-    def validValues = ["TRUE", "ALL", "INFO"]
-
-    if (validValues.contains(property_ENABLE_LOGGING.toUpperCase())) {
-        Boolean messageLogged = create_attachment(message, "Payload")
-    }
-
-    return message;
-}
-
-/*
-log payload depending on property.ENABLE_LOGGING
-*/
-def Message payload_logger_v2(Message message) {
-    map = message.getProperties();
-    property_ENABLE_LOGGING = map.get("EnableLogging");
-    message.setHeader("SAP_IsIgnoreProperties", new Boolean(true));
-
-    if (property_ENABLE_LOGGING.toUpperCase().equals("TRUE")) {
-        def currentStep = message.getHeaders().get("CamelCxfMessage").getProcessingStep();
-        def activityName = currentStep.getName();
-        def position = currentStep.getProcessingStepPosition();
-
-        def body = message.getBody(java.lang.String) as String;
-        def messageLogFactory = messageLogFactory;
-        def messageLog = messageLogFactory.getMessageLog(message);
-
-        String contentType = message.getHeaders().get("Content-Type");
-
-        messageLog.addAttachmentAsString("Payload_" + activityName + "_" + position, body, contentType);
-    }
-
-    return message;
-}
-
-
-
-
-def Message select_attachment(Message message) {
-    
-   Map<String, DataHandler> attachments = message.getAttachments()
-   if (attachments.isEmpty()) {
-      throw new Exception ("No content in Attachment")
-   } else {
-      Iterator<DataHandler> it = attachments.values().iterator()
-      DataHandler attachment = it.next()
-      message.setBody(attachment.getContent())
-   }
-   
-   return message
-   
-}
 
 //-----------------------------------------------------------
 // Log Sub Routines
 //-----------------------------------------------------------
-def Message log(String prefix, Message message,boolean logPayload, boolean logHeaders, boolean logProperties, boolean logSysEnv) {
-
-    def headers = message.getHeaders();
-    def properties = message.getProperties();
-
-    def enableLoggingValue = properties.get("EnableLogging", "TRUE");
-    boolean property_ENABLE_LOGGING = enableLoggingValue?.toString()?.equalsIgnoreCase("true");
-
-    if (property_ENABLE_LOGGING) {
-        
-        message.setHeader("SAP_IsIgnoreProperties", new Boolean(true));
-        StringBuffer logInfo = new StringBuffer()
-        int logNumber = headers.get("log.Number", "1") as Integer;
-
-        if (prefix == "") {
-            property_prefix = properties.get("log.prefix");
-           
-            if (property_prefix == null || property_prefix.trim() == "") {
-                property_prefix = "Payload"
-            }    
-        } else {
-            property_prefix = prefix
-        }    
-        
-        property_prefix = String.format("%04d", logNumber) + "." + property_prefix
-    
-        // Define Attachment Name
-        def attachmentName = determineAttachmentName(property_prefix, "");
-
-        //Prepare content, Header Log
-        if(logHeaders) {
-            logInfo.append(logMessageHeaders(message));
-        } 
-
-        //Prepare contentm Property Log
-        if(logProperties) {
-            logInfo.append(logMessageProperties(message));
-        } 
-
-        //Prepare contentm Property Log
-        if(logSysEnv) {
-            logInfo.append(logSystemEnvironment(message));
-        } 
-
-        def body = message.getBody(java.lang.String) as String;
-        def messageLog = messageLogFactory.getMessageLog(message);
-
-        if(logPayload) {
-            if (logHeaders || logProperties || logSysEnv) {
-                logInfo.append("Payload" + "\n--------------------------\n")
-            }    
-            logInfo.append(body);
-        }
-        
-        logNumber++;
-        message.setHeader("log.Number", logNumber);
-
-        //String contentType = message.getHeaders().get("Content-Type");
-        String contentType = determineContentType(logInfo.toString());
-        messageLog.addAttachmentAsString(attachmentName, logInfo.toString(), contentType);
-
-    }
-
-    return message;
-}
-
-
-
 def String logMessageHeaders(Message message) {
     
     def headers = message.getHeaders();
@@ -462,4 +329,18 @@ def Message increaseLogNumber(Message message) {
     return message
 }
 
+def Message select_attachment(Message message) {
+    
+   Map<String, DataHandler> attachments = message.getAttachments()
+   if (attachments.isEmpty()) {
+      throw new Exception ("No content in Attachment")
+   } else {
+      Iterator<DataHandler> it = attachments.values().iterator()
+      DataHandler attachment = it.next()
+      message.setBody(attachment.getContent())
+   }
+   
+   return message
+   
+}
 

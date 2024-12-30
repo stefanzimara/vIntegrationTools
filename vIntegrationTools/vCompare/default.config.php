bb6107806d01d4a1818ecfd6653cfebb04c3266a
stefanzimara <129875168+stefanzimara@users.noreply.github.com>
2024-12-30 18:12:05 +0100

    Default File Ananomyized

--- vIntegrationTools/vCompare/default.config.php
+++ vIntegrationTools/vCompare/default.config.php
@@ -1,11 +1,11 @@
 <?php
 
-    $config[0]["owner"] = "Franke";
-    $config[0]["system"] = "FQL_Q";
-    $config[0]["clientid"] = "sb-0673c4e7-b9c2-4fdd-be18-0e14296675aa!b102732|it!b16077";
-    $config[0]["clientsecret"] = "8123be78-58f5-4ec2-8b9c-16a9df8ecd28$0BYfITCdFDiTVjF5wW3lLeBSsWM0SOfGyk2bBpKb2ls=";
-    $config[0]["tokenurl"] = "https://fclq001.authentication.eu10.hana.ondemand.com/oauth/token";
-    $config[0]["baseurl"] = "fclq001.it-cpi001.cfapps.eu10.hana.ondemand.com";
+    $config[0]["owner"] = "MyCompany";
+    $config[0]["system"] = "TrialTenant";
+    $config[0]["clientid"] = "12345678919";
+    $config[0]["clientsecret"] = "ABC-Secret";
+    $config[0]["tokenurl"] = "https://myHost/oauth/token";
+    $config[0]["baseurl"] = "myBaseUrl-Domain";
 
 
 ?>

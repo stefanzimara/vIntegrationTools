19f8f594c64eb147ec2a419178e825df4320cdbf
stefanzimara <129875168+stefanzimara@users.noreply.github.com>
2025-03-12 13:50:46 +0100

    New Software Archiutecture
    New Architecture for soliution

---
+++ vIntegrationTools/vCompare/vnexus.config.php
@@ -0,0 +1,24 @@
+<?php
+
+    $config[0]["owner"] = "Global IT";
+    $config[0]["system"] = "DEV";
+    $config[0]["clientid"] = "sb-89bce982-914b-42d5-afbc-ef36286f2176!b22101|it!b259";
+    $config[0]["clientsecret"] = "ea6cc975-a9ca-456f-a3da-d5d8edf4ac94\$hdkzGxvBuE0SHv_Ta1EadTttDNW5WBXgDTH-p38mrRU=";
+    $config[0]["tokenurl"] = "https://galenica-oc-development.authentication.eu20.hana.ondemand.com/oauth/token";
+    $config[0]["baseurl"] = "galenica-oc-development.it-cpi005.cfapps.eu20.hana.ondemand.com";
+
+    $config[1]["owner"] = "Global IT";
+    $config[1]["system"] = "QAS";
+    $config[1]["clientid"] = "sb-6c329ea2-66a0-48da-af74-e7e28ff3bb99!b53797|it!b18631";
+    $config[1]["clientsecret"] = "c6bf80e9-95c7-47af-a014-895b8f9f2661\$I4rOfX5YuGfvnT6830QaMd7p-T7xBcEKKyMFSdGfMSE=";
+    $config[1]["tokenurl"] = "https://galenica-oc-qualityassurance.authentication.eu20.hana.ondemand.com/oauth/token";
+    $config[1]["baseurl"] = "galenica-oc-qualityassurance.it-cpi023.cfapps.eu20-001.hana.ondemand.com";
+
+    $config[2]["owner"] = "Global IT";
+    $config[2]["system"] = "PROD";
+    $config[2]["clientid"] = "sb-78ae71d6-e290-4901-907f-1448e4f99e57!b45482|it!b259";
+    $config[2]["clientsecret"] = "1ad51498-a75f-4a85-b9f0-0fc251237c26\$vLMNyZyerYg6a8Jjn36hZKo-sH9HDXpM1SXLn_jBtB0=";
+    $config[2]["tokenurl"] = "https://galenica-oc-production.authentication.eu20.hana.ondemand.com/oauth/token";
+    $config[2]["baseurl"] = "galenica-oc-production.it-cpi005.cfapps.eu20.hana.ondemand.com";
+
+?>
